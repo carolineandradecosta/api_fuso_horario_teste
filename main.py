57f214ef5b5afe1fc634d0b5a7bee5467bff2727
Caroline Andrade <andradecostacaroline@gmail.com>
2023-04-22 16:55:35 -0300

    melhorando o código final

--- main.py
+++ main.py
@@ -6,17 +6,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/fusos/{fuso_horario:path}')
-def home(fuso_horario: str):
-    return {fuso_horario: datetime.now().astimezone(timezone(fuso_horario)).strftime('%Z %H:%M')}
-
-#  http://127.0.0.1:8000/fusos/Brazil/Acre
-#  http://127.0.0.1:8000/fusos/Brazil/east
-
-# @app.get('/fusos/{fuso_horario:path}/fusos2/{fuso_horario2:path}')
-# def home(fuso_horario: str, fuso_horario2: str):
-#        return \
-#            [{fuso_horario: datetime.now().astimezone(timezone(fuso_horario)).strftime('%Z %H:%M')},
-#             {fuso_horario2: datetime.now().astimezone(timezone(fuso_horario2)).strftime('%Z %H:%M')}]
+@app.get('/fusos/{fuso_horario:path}/fusos2/{fuso_horario2:path}')
+def hora_fuso_horario(fuso_horario: str, fuso_horario2: str):
+    return \
+        [{fuso_horario: datetime.now().astimezone(timezone(fuso_horario)).strftime('%Z %H:%M')},
+         {fuso_horario2: datetime.now().astimezone(timezone(fuso_horario2)).strftime('%Z %H:%M')}]
 
 #  http://127.0.0.1:8000/fusos/Brazil/Acre/fusos2/Brazil/east
